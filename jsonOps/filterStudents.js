21bf9be1971406fd14a272275d3795f123880977
Sainathgit config --global user.name Sainathgit config --global user.name Sainath <sainath.valavala.31@gmail.com>
2025-10-16 12:34:08 +0530

    added e parametarized routing using express

---
+++ jsonOps/filterStudents.js
@@ -0,0 +1,27 @@
+var express = require("express");
+const app = express();
+var studnets = require("./students.json");
+var products = require("./products.json");
+// console.log(studnets);
+app.get("/", function (req, res) {
+  res.send("server created to filter students from students.json");
+});
+app.get("/b/:n", function (req, res) {
+  var sname = studnets.find((x) => {
+    if (x.name == req.params.n) {
+      return true;
+    }
+  });
+  res.send(sname);
+});
+app.get("/a/:n", function (req, res) {
+  var pname = products.find((x) => {
+    if (x.name == req.params.n) {
+      return true;
+    }
+  });
+  res.send(pname);
+});
+app.listen(2000, function () {
+  console.log("app is running on portno:2000");
+});
