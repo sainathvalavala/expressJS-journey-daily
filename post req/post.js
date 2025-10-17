afc36f5d05331376bea91db246fe5b74badfd171
Sainathgit config --global user.name Sainathgit config --global user.name Sainath <sainath.valavala.31@gmail.com>
2025-10-17 11:41:12 +0530

    updated

--- post req/post.js
+++ post req/post.js
@@ -1,24 +1,48 @@
+// const express = require("express");
+// const fs = require("fs");
+// const bodyparser = require("body-parser");
+// const app = express();
+
+// app.use(express.static(__dirname + "/staticFiles"));//search static files
+// app.get("/addData", function (req, res) {
+//   console.log(req.query);
+//   res.send("received Get Request");
+// });
+// app.use(bodyparser.urlencoded({ extended: false }));//body parser
+// var arrayData = [];
+// app.post("/addData", function (req, res) {
+//   const data = req.body;
+//   arrayData.push(data);
+//   console.log(data);
+//   console.log(arrayData);
+//   fs.writeFileSync("staticFiles/file.txt", JSON.stringify(arrayData));
+//   res.send("sent Post Request");
+// });
+
+// app.listen(3001, () => {
+//   console.log("server running on portno:3001");
+// });
+
 const express = require("express");
-const fs = require("fs");
-const bodyparser = require("body-parser");
 const app = express();
-
-app.use(express.static(__dirname + "/staticFiles"));//search static files
-app.get("/addData", function (req, res) {
-  console.log(req.query);
-  res.send("received Get Request");
+const fs = require("fs");
+app.get("/", (req, res) => {
+  res.send("request /form.html");
 });
-app.use(bodyparser.urlencoded({ extended: false }));//body parser
-var arrayData = [];
-app.post("/addData", function (req, res) {
-  const data = req.body;
-  arrayData.push(data);
-  console.log(data);
-  console.log(arrayData);
-  fs.writeFileSync("staticFiles/file.txt", JSON.stringify(arrayData));
-  res.send("sent Post Request");
+app.use(express.static(__dirname + "/staticfiles"));
+let arrData = [];
+
+const bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({ extended: false }));
+app.post("/addData", (req, res) => {
+  const bdata = req.body;
+  arrData.push(bdata);
+  console.log(bdata);
+  console.log(arrData);
+  fs.writeFileSync("staticFiles/file.txt", JSON.stringify(arrData));
+  res.send("post request sent");
 });
 
-app.listen(3001, () => {
-  console.log("server running on portno:3001");
+app.listen(2001, () => {
+  console.log("server is running on port no:2001");
 });
